94c3abf1103598bbd6d866775092d5dc36f6365c
Abener OChoa <ochoa8al@gmail.com>
2024-01-31 20:02:36 -0600

    Formularios reactivos

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
+
+  miFormulario: FormGroup;
+
+  constructor( private fb : FormBuilder){
+    this.miFormulario = fb.group({
+      nombre: ['',[ Validators.required,Validators.minLength(3)]],
+      correo: ['',[Validators.email, Validators.required]]
+    })
+  }
+
+  Enviar(){
+    console.log(this.miFormulario);
+  };
+
+  hayError(campo:string,validacion:string){
+    return this.miFormulario.get(campo)?.hasError(validacion) &&
+      this.miFormulario.get(campo)?.touched    
+  }
 
 }
